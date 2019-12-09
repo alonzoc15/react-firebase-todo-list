feb1beef4bab6cd971a2f5e3fa49d3d8c8bdeeee
Alonzo Camille <alonzocamille15@gmail.com>
2019-12-09 18:06:17 -0500

    simple todo-list app made in react and firebase

---
+++ src/Config/config.js
@@ -0,0 +1,10 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyC03dOlkyXu8-2aiisn4Opzou3uR6Wb1r0",
+    authDomain: "react-todo-182f6.firebaseapp.com",
+    databaseURL: "https://react-todo-182f6.firebaseio.com",
+    projectId: "react-todo-182f6",
+    storageBucket: "react-todo-182f6.appspot.com",
+    messagingSenderId: "590224049800",
+    appId: "1:590224049800:web:e2a93c169aa4195115813d",
+    measurementId: "G-D26EH5TW7L"
+};
